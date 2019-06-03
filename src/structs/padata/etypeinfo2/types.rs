9092f22afb8609f195194294d05746024a73f777
Zer1t0 <Zer1t0@mail.com>
2019-06-03 08:15:14 +0200

    moved etypeinfo2 into its own module

---
+++ src/structs/padata/etypeinfo2/types.rs
@@ -0,0 +1,6 @@
+pub const AES256_CTS_HMAC_SHA1_96: i32 = 18;
+pub const AES128_CTS_HMAC_SHA1_96: i32 = 17;
+pub const RC4_HMAC: i32 = 23;
+pub const RC4_HMAC_EXP: i32 = 24;
+pub const DES_CBC_MD5: i32 = 3;
+pub const DES_CBC_CRC: i32 = 1;
