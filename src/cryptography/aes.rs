7b8c5a784ebe87abe3bf947674d9c3be27fac2cc
Zer1t0 <Zer1t0@mail.com>
2019-06-15 14:08:07 +0200

    refactorized aes_keys_generation

--- src/cryptography/aes.rs
+++ src/cryptography/aes.rs
@@ -33,38 +33,46 @@ impl AesSizes {
             AesSizes::Aes256 => {return aes::KeySize::KeySize256}
         }
     }
-
 }
 
-pub fn generate_aes_128_key(key: &str, salt: &[u8]) -> Vec<u8> {
-    return generate_aes_key(key, salt, AesSizes::Aes128);
+pub fn generate_aes_128_key(key: &[u8], salt: &[u8]) -> Vec<u8> {
+    return generate_aes_key(key, salt, &AesSizes::Aes128);
 }
 
-pub fn generate_aes_256_key(key: &str, salt: &[u8]) -> Vec<u8> {
-    return generate_aes_key(key, salt, AesSizes::Aes256);
+pub fn generate_aes_256_key(key: &[u8], salt: &[u8]) -> Vec<u8> {
+    return generate_aes_key(key, salt, &AesSizes::Aes256);
 }
 
-fn generate_aes_key(key: &str, salt: &[u8], aes_sizes: AesSizes) -> Vec<u8> {
-    let iteration_count = 0x1000;
-    let mut hmacker = Hmac::new(Sha1::new(), key.as_bytes());
-    let mut seed : Vec<u8> = vec![0; aes_sizes.seed_size()];
-    pbkdf2(&mut hmacker, salt, iteration_count, &mut seed);
+fn generate_aes_key(key: &[u8], salt: &[u8], aes_sizes: &AesSizes) -> Vec<u8> {
+    let seed = pbkdf2_sha1(key, salt, aes_sizes.seed_size());
 
     let mut plaintext = n_fold("kerberos".as_bytes(), aes_sizes.block_size());
     let mut result : Vec<u8> = Vec::new();
 
     while result.len() < aes_sizes.seed_size() {
-        let mut encryptor = aes::cbc_encryptor(aes_sizes.key_size(), &seed, 
-            &vec![0; aes_sizes.block_size()], blockmodes::NoPadding);
-
-        let mut ciphertext: Vec<u8> = vec![0; aes_sizes.block_size()];
-        encryptor.encrypt(&mut RefReadBuffer::new(&plaintext),
-                            &mut RefWriteBuffer::new(&mut ciphertext), true).unwrap();
-        
-        plaintext = ciphertext.clone();
-        result.append(&mut ciphertext);
+        plaintext = encrypt_with_aes_cbc(&plaintext, &seed, &aes_sizes);
+        result.append(&mut plaintext.clone());
     }
     return result;
+}
+
+fn pbkdf2_sha1(key: &[u8], salt: &[u8], seed_size: usize) -> Vec<u8> {
+    let iteration_count = 0x1000;
+    let mut hmacker = Hmac::new(Sha1::new(), key);
+    let mut seed : Vec<u8> = vec![0; seed_size];
+    pbkdf2(&mut hmacker, salt, iteration_count, &mut seed);
+    return seed;
+}
+
+fn encrypt_with_aes_cbc(plaintext: &[u8], seed: &[u8], aes_sizes: &AesSizes) -> Vec<u8> {
+    let mut encryptor = aes::cbc_encryptor(aes_sizes.key_size(), &seed, 
+            &vec![0; aes_sizes.block_size()], blockmodes::NoPadding);
+
+    let mut ciphertext: Vec<u8> = vec![0; aes_sizes.block_size()];
+    encryptor.encrypt(&mut RefReadBuffer::new(plaintext),
+                        &mut RefWriteBuffer::new(&mut ciphertext), true).unwrap();
+    
+    return ciphertext;
 }
 
 fn n_fold(v: &[u8], nbytes: usize) -> Vec<u8> {
@@ -93,7 +101,7 @@ fn rotate_rigth_n_bits(v: &[u8], nbits: usize) -> Vec<u8> {
 
     for i in 0..v.len() {
         let index_a = (((i as i32 - nbytes as i32) % v.len() as i32) + v.len() as i32) as usize % v.len();
-        let index_b = (((i as i32 - nbytes as i32 - 1) % v.len() as i32) + v.len() as i32) as usize % v.len() ;
+        let index_b = (((i as i32 - nbytes as i32 - 1) % v.len() as i32) + v.len() as i32) as usize % v.len();
 
         v_rotate.push(
             ((((v[index_a] as u16) >> nbits_remain)) as u8) | 
@@ -118,26 +126,17 @@ fn divide_in_n_bytes_chunks(v: &[u8], nbytes: usize) -> Vec<Vec<u8>> {
 
 fn add_chunks_with_1s_complement_addition(chunks: &Vec<Vec<u8>>) -> Vec<u8> {
     let mut result = chunks[0].clone();
-
     for chunk in chunks[1..].iter() {
         result = add_chunk_with_1s_complement(&result, chunk);
     }
-
     return result;
 }
 
 fn add_chunk_with_1s_complement(chunk_1: &Vec<u8>, chunk_2: &Vec<u8>) -> Vec<u8> {
-    let chunk_len = chunk_1.len();
     let mut tmp_add = add_chunks_as_u16_vector(chunk_1, chunk_2);
 
     while tmp_add.iter().any(|&x| x > 0xff) {
-        let mut aux_vector: Vec<u16> = vec![0; chunk_len];
-
-        for i in 0..tmp_add.len() {
-            let index = (((i as i32 - chunk_len as i32 + 1) % tmp_add.len() as i32) + tmp_add.len() as i32) as usize % tmp_add.len();
-            aux_vector[i] = (tmp_add[index] >> 8) + (tmp_add[i] & 0xff)
-        }
-        tmp_add = aux_vector;
+        propagate_carry_bits(&mut tmp_add);    
     }
 
     return convert_u16_vector_to_u8_vector(&tmp_add);
@@ -153,12 +152,20 @@ fn add_chunks_as_u16_vector(chunk_1: &Vec<u8>, chunk_2: &Vec<u8>) -> Vec<u16> {
     return tmp_add;
 }
 
-fn convert_u16_vector_to_u8_vector(v: &Vec<u16>) -> Vec<u8> {
-    let mut v_8 : Vec<u8> = Vec::with_capacity(v.len());
-    for item in v.iter() {
-        v_8.push(*item as u8);
+fn propagate_carry_bits(tmp_add: &mut Vec<u16>) {
+    let mut aux_vector: Vec<u16> = vec![0; tmp_add.len()];
+
+    for i in 0..tmp_add.len() {
+        let index = (((i as i32 - tmp_add.len() as i32 + 1) % tmp_add.len() as i32) + tmp_add.len() as i32) as usize % tmp_add.len();
+        aux_vector[i] = (tmp_add[index] >> 8) + (tmp_add[i] & 0xff)
     }
-    return v_8;
+    for i in 0..tmp_add.len() {
+        tmp_add[i] = aux_vector[i];
+    }
+}
+
+fn convert_u16_vector_to_u8_vector(v: &Vec<u16>) -> Vec<u8> {
+    return v.iter().map(|&x| x as u8).collect();
 }
 
 
@@ -172,19 +179,19 @@ mod test {
         assert_eq!(vec![
             0x61, 0x7f, 0x72, 0xfd, 0xbc, 0x85, 0x1c, 0x45,
             0x9a, 0x1c, 0x39, 0xbf, 0x83, 0x23, 0x56, 0x09],
-            generate_aes_128_key("Minnie1234", "KINGDOM.HEARTSmickey".as_bytes()));
+            generate_aes_128_key("Minnie1234".as_bytes(), "KINGDOM.HEARTSmickey".as_bytes()));
 
         assert_eq!(vec![0x19, 0xa9, 0xb7, 0xc0, 0x87, 0xf8, 0xfd, 0x3d, 
                         0xf5, 0x94, 0xaf, 0xd2, 0xc7, 0xf6, 0x64, 0x73],
-            generate_aes_128_key("Sora1234", "KINGDOM.HEARTSroxas".as_bytes()));
+            generate_aes_128_key("Sora1234".as_bytes(), "KINGDOM.HEARTSroxas".as_bytes()));
 
         assert_eq!(vec![0x83, 0x91, 0x03, 0x34, 0x5b, 0x8b, 0x01, 0x69, 
                         0xb6, 0xff, 0x47, 0xe6, 0x14, 0xe6, 0xce, 0xec],
-            generate_aes_128_key("Kairi1234", "KINGDOM.HEARTSnamine".as_bytes()));
+            generate_aes_128_key("Kairi1234".as_bytes(), "KINGDOM.HEARTSnamine".as_bytes()));
 
         assert_eq!(vec![0x66, 0x6b, 0x33, 0x75, 0x4d, 0x8b, 0xdd, 0x0e, 
                         0xd2, 0x58, 0x99, 0x93, 0xcb, 0xcc, 0x77, 0x8e],
-            generate_aes_128_key("Roxas1234", "KINGDOM.HEARTSxion".as_bytes()));
+            generate_aes_128_key("Roxas1234".as_bytes(), "KINGDOM.HEARTSxion".as_bytes()));
     }
 
     #[test]
@@ -193,25 +200,25 @@ mod test {
                         0x26, 0xa5, 0x69, 0xf8, 0xb7, 0xc3, 0x67, 0x15, 
                         0xc8, 0xda, 0xef, 0x10, 0x9f, 0xa3, 0xd8, 0xb2, 
                         0xe1, 0x46, 0x16, 0xaa, 0xca, 0xb5, 0x49, 0xfd],
-            generate_aes_256_key("Minnie1234", "KINGDOM.HEARTSmickey".as_bytes()));
+            generate_aes_256_key("Minnie1234".as_bytes(), "KINGDOM.HEARTSmickey".as_bytes()));
 
         assert_eq!(vec![0x99, 0xc8, 0x9e, 0xd5, 0x12, 0x19, 0x84, 0xc8, 
                         0xe4, 0x12, 0xc1, 0xa7, 0xc1, 0x34, 0x69, 0x0f, 
                         0x61, 0xc9, 0x55, 0x48, 0x38, 0x8b, 0xba, 0x20, 
                         0x8f, 0xd9, 0xd9, 0x98, 0x6c, 0x8f, 0x77, 0xd6],
-            generate_aes_256_key("Sora1234", "KINGDOM.HEARTSroxas".as_bytes()));
+            generate_aes_256_key("Sora1234".as_bytes(), "KINGDOM.HEARTSroxas".as_bytes()));
 
         assert_eq!(vec![0x86, 0x95, 0xf8, 0x2e, 0x92, 0x0e, 0x0a, 0xce, 
                         0x42, 0x9b, 0x8d, 0x9e, 0xf6, 0x19, 0x67, 0x0d, 
                         0x03, 0x67, 0x43, 0x0c, 0x12, 0xb3, 0x23, 0x3f, 
                         0x32, 0xc5, 0xc8, 0x9f, 0xb7, 0xd6, 0x04, 0xff],
-            generate_aes_256_key("Kairi1234", "KINGDOM.HEARTSnamine".as_bytes()));
+            generate_aes_256_key("Kairi1234".as_bytes(), "KINGDOM.HEARTSnamine".as_bytes()));
 
         assert_eq!(vec![0xd6, 0x52, 0xf8, 0x5d, 0x86, 0x5d, 0x92, 0xe3, 
                         0x47, 0xf6, 0xa5, 0x6b, 0x63, 0x5d, 0x31, 0x02, 
                         0x3b, 0x92, 0x65, 0x1a, 0x00, 0x2c, 0x05, 0x9d, 
                         0x8d, 0xb8, 0xee, 0x2a, 0x06, 0xd2, 0x65, 0x0f],
-            generate_aes_256_key("Roxas1234", "KINGDOM.HEARTSxion".as_bytes()));
+            generate_aes_256_key("Roxas1234".as_bytes(), "KINGDOM.HEARTSxion".as_bytes()));
     }
 
 
