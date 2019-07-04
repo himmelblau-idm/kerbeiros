2e40c108db684ef9fa89d51fd60a74d2e458ffe3
Zer1t0 <Zer1t0@mail.com>
2019-07-04 07:18:06 +0200

    added decrypt rc4

--- src/crypter/crypters/rc4.rs
+++ src/crypter/crypters/rc4.rs
@@ -40,7 +40,7 @@ impl KerberosCrypter for RC4Crypter {
     }
 
     fn decrypt(&self, key: &[u8], ciphertext: &[u8]) -> KerberosResult<Vec<u8>> {
-        unimplemented!();
+        return decrypt_rc4_hmac_md5(key, ciphertext);
     }
 
     fn encrypt(&self, key: &[u8], plaintext: &[u8]) -> KerberosResult<Vec<u8>> {
@@ -85,6 +85,34 @@ mod test {
         );
     }
 
+    #[test]
+    fn test_decrypt_timestamp_rc4_hmac_md5() {
+        let timestamp = PaEncTsEnc::from_datetime(Utc.ymd(2019, 6, 4).and_hms_micro(06, 13, 52, 016747)).unwrap();
+        let timestamp_raw = timestamp.asn1_type().encode().unwrap();
+
+        let mut rc4_crypter = RC4Crypter::new();
+        rc4_crypter._set_preamble(&[0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38]);
+
+        let ciphertext = vec![
+            0x8f, 0x24, 0x62, 0xd7, 0x70, 0xa7, 0xce, 0x9e, 
+            0x5b, 0x5e, 0xe6, 0x35, 0xd8, 0xbc, 0x54, 0x9a,
+            0x83, 0xb0, 0x93, 0xcf, 0xe2, 0x6b, 0x55, 0x25,
+            0xb7, 0x83, 0x33, 0x89, 0x35, 0xd1, 0xa9, 0xf2, 
+            0x8d, 0x48, 0xde, 0x78, 0xfe, 0x40, 0xf1, 0x22, 
+            0xb2, 0xec, 0xe5, 0x9a, 0x6f, 0x43, 0xfb, 0x14, 
+            0xaa, 0x03, 0x22
+        ];
+
+        assert_eq!(timestamp_raw, 
+            rc4_crypter.generate_key_from_password_and_decrypt(
+                "test",
+                &Vec::new(),
+                &ciphertext
+            ).unwrap()
+        );
+    }
+
+
     fn ntlm_hash(password: &str) -> Vec<u8> {
         return RC4Crypter::new().generate_key_from_password(password, &Vec::new());
     }
@@ -105,6 +133,5 @@ mod test {
                    ntlm_hash("123456789"));
     
     }
-
 
 }
--- src/crypter/rc4hmacmd5.rs
+++ src/crypter/rc4hmacmd5.rs
@@ -1,4 +1,5 @@
 pub use super::cryptography::*;
+use crate::error::*;
 
 pub fn encrypt_rc4_hmac_md5(key: &[u8], timestamp: &[u8], preamble: &[u8]) -> Vec<u8> {
     let mut plaintext : Vec<u8> = Vec::new();
@@ -13,4 +14,25 @@ pub fn encrypt_rc4_hmac_md5(key: &[u8], timestamp: &[u8], preamble: &[u8]) -> Ve
     cksum.append(&mut enc);
 
     return cksum;
+}
+
+
+pub fn decrypt_rc4_hmac_md5(key: &[u8], ciphertext: &[u8]) -> KerberosResult<Vec<u8>> {
+    if ciphertext.len() < 24 {
+        return Err(KerberosCryptographyErrorKind::DecryptionError("Ciphertext too short".to_string()))?;
+    }
+
+    let cksum = &ciphertext[0..16];
+    let basic_ciphertext = &ciphertext[16..];
+    let ki = hmac_md5(key, &[1, 0, 0 ,0]);
+    let ke = hmac_md5(&ki, &cksum);
+    let plaintext = rc4_decrypt(&ke, &basic_ciphertext);
+
+    let  plaintext_cksum = hmac_md5(&ki, &plaintext);
+
+    if cksum != &plaintext_cksum[..] {
+        return Err(KerberosCryptographyErrorKind::DecryptionError("Hmac integrity failure".to_string()))?;
+    }
+
+    return Ok(plaintext[8..].to_vec());
 }
