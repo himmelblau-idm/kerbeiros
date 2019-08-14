357e4cc981b7db4ee3ce8e5445299f2d3cd2e9fb
Zer1t0 <Zer1t0@mail.com>
2019-08-14 07:22:05 +0200

    changed new to from in PaEncTsEncAsn1

--- src/types/asn1/padata/enctimestamp.rs
+++ src/types/asn1/padata/enctimestamp.rs
@@ -23,11 +23,7 @@ impl PaEncTsEnc {
     }
 
     pub fn build(&self) -> Vec<u8> {
-        return self.asn1_type().encode().unwrap();
-    }
-
-    pub(crate) fn asn1_type(&self) -> PaEncTsEncAsn1 {
-        return PaEncTsEncAsn1::new(self);
+        return PaEncTsEncAsn1::from(self).encode().unwrap();
     }
 
 }
@@ -58,7 +54,17 @@ pub(crate) struct PaEncTsEncAsn1 {
 
 impl PaEncTsEncAsn1 {
 
-    fn new(pa_enc_ts_enc: &PaEncTsEnc) -> Self {
+    fn default() -> Self {
+        return Self {
+            patimestamp: SeqField::default(),
+            pausec: SeqField::default()
+        }
+    }
+
+}
+
+impl From<&PaEncTsEnc> for PaEncTsEncAsn1 {
+    fn from(pa_enc_ts_enc: &PaEncTsEnc) -> Self {
         let mut pa_enc_ts_enc_asn1 = Self::default();
 
         pa_enc_ts_enc_asn1.set_patimestamp(pa_enc_ts_enc.patimestamp.clone().into());
@@ -69,14 +75,6 @@ impl PaEncTsEncAsn1 {
 
         return pa_enc_ts_enc_asn1;
     }
-
-    fn default() -> Self {
-        return Self {
-            patimestamp: SeqField::default(),
-            pausec: SeqField::default()
-        }
-    }
-
 }
 
 #[cfg(test)]
@@ -94,7 +92,7 @@ mod test {
                             0xa0, 0x11, 0x18, 0x0f, 0x32, 0x30, 0x31, 0x39, 0x30, 0x36, 
                                 0x30, 0x34, 0x30, 0x35, 0x32, 0x32, 0x31, 0x32, 0x5a, 
                             0xa1, 0x05, 0x02, 0x03, 0x02, 0x31, 0x6d],
-                pa_enc_ts_enc.asn1_type().encode().unwrap());
+                PaEncTsEncAsn1::from(&pa_enc_ts_enc).encode().unwrap());
 
     }
 
