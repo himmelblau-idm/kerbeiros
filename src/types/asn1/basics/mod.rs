45fdda386982dd5c7cf712e8f72a6de5ae0f7f02
Zer1t0 <Zer1t0@mail.com>
2019-08-20 11:05:02 +0200

    moved simple asn1 types to primitivess module

--- src/types/asn1/basics/mod.rs
+++ src/types/asn1/basics/mod.rs
@@ -1,17 +1,2 @@
-pub mod int32;
-pub use int32::*;
-
-pub mod uint32;
-pub use uint32::*;
-
-pub mod microseconds;
-pub use microseconds::*;
-
-pub mod kerberos_flags;
-pub use kerberos_flags::*;
-
-pub mod kerberos_time;
-pub use kerberos_time::*;
-
-pub mod kerberos_string;
-pub use kerberos_string::*;
+mod primitives;
+pub use primitives::*;
