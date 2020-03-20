8505124a5b0cb57da7957bfc3311554513d8f8cb
Eloy <zer1t0ps@protonmail.com>
2020-03-20 18:24:00 +0100

    style: format byteparser module with cargo fmt

--- src/byteparser/mod.rs
+++ src/byteparser/mod.rs
@@ -1,6 +1,5 @@
-
 pub fn u16_array_to_le_bytes(u16_array: &[u16]) -> Vec<u8> {
-    let mut u8_vec : Vec<u8> = Vec::with_capacity(u16_array.len() * 2);
+    let mut u8_vec: Vec<u8> = Vec::with_capacity(u16_array.len() * 2);
 
     for u16_item in u16_array.iter() {
         let u8_min = *u16_item as u8;
@@ -14,16 +13,22 @@ pub fn u16_array_to_le_bytes(u16_array: &[u16]) -> Vec<u8> {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn test_u16_array_to_le_bytes() {
-        assert_eq!(vec![0,0], u16_array_to_le_bytes(&[0]));
-        assert_eq!(vec![1,0], u16_array_to_le_bytes(&[1]));
-        assert_eq!(vec![9,0,8,0,7,0,6,0], u16_array_to_le_bytes(&[9,8,7,6]));
-        assert_eq!(vec![0x15,0x03], u16_array_to_le_bytes(&[789]));
-        assert_eq!(vec![0x00,0x01], u16_array_to_le_bytes(&[256]));
-        assert_eq!(vec![0xd2,0x04, 0xa5, 0x03, 0xbe, 0x6c], u16_array_to_le_bytes(&[1234, 933, 27838]));
+        assert_eq!(vec![0, 0], u16_array_to_le_bytes(&[0]));
+        assert_eq!(vec![1, 0], u16_array_to_le_bytes(&[1]));
+        assert_eq!(
+            vec![9, 0, 8, 0, 7, 0, 6, 0],
+            u16_array_to_le_bytes(&[9, 8, 7, 6])
+        );
+        assert_eq!(vec![0x15, 0x03], u16_array_to_le_bytes(&[789]));
+        assert_eq!(vec![0x00, 0x01], u16_array_to_le_bytes(&[256]));
+        assert_eq!(
+            vec![0xd2, 0x04, 0xa5, 0x03, 0xbe, 0x6c],
+            u16_array_to_le_bytes(&[1234, 933, 27838])
+        );
     }
 }
