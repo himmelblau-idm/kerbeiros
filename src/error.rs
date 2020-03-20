2fb868e5c38d4d4f31ed3a571942ca51859a1474
Eloy <zer1t0ps@protonmail.com>
2020-03-20 18:24:43 +0100

    style: format error module with cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,12 @@
 //! Errors raised by this library
 
-use std::fmt;
-use std::result;
+use crate::messages::*;
 use ascii::FromAsciiError;
 use failure::*;
 use failure_derive::Fail;
 use red_asn1;
-use crate::messages::*;
+use std::fmt;
+use std::result;
 
 /// Result to wrap kerbeiros error.
 pub type Result<T> = result::Result<T, Error>;
@@ -14,18 +14,18 @@ pub type Result<T> = result::Result<T, Error>;
 /// Error returned by functions of the kerbeiros library.
 #[derive(Debug)]
 pub struct Error {
-    inner: Context<ErrorKind>
+    inner: Context<ErrorKind>,
 }
 
 /// Type of error in kerbeiros library.
 #[derive(Clone, PartialEq, Debug, Fail)]
 pub enum ErrorKind {
     /// Error handlening asn1 entities.
-    #[fail (display = "Asn1 error: {}", _0)]
+    #[fail(display = "Asn1 error: {}", _0)]
     Asn1Error(red_asn1::ErrorKind),
 
     /// Error produced in the application of cryptographic algorithms.
-    #[fail (display = "Cryptography error: {}", _0)]
+    #[fail(display = "Cryptography error: {}", _0)]
     CryptographyError(Box<CryptographyErrorKind>),
 
     /// Invalid ascii string.
@@ -35,25 +35,27 @@ pub enum ErrorKind {
     /// Invalid microseconds value. Minimum = 0, Maximum = 999999.
     #[fail(display = "Invalid microseconds value {}. Max is 999999", _0)]
     InvalidMicroseconds(u32),
-    
+
     /// Error in i/o operation.
-    #[fail (display = "Error in i/o operation")]
+    #[fail(display = "Error in i/o operation")]
     IOError,
 
     /// Invalid key
-    #[fail (display = "Invalid key: Only hexadecimal characters are allowed [1234567890abcdefABCDEF]")] 
+    #[fail(
+        display = "Invalid key: Only hexadecimal characters are allowed [1234567890abcdefABCDEF]"
+    )]
     InvalidKeyCharset,
 
     /// Invalid key
-    #[fail (display = "Invalid key: Length should be {}", _0)] 
+    #[fail(display = "Invalid key: Length should be {}", _0)]
     InvalidKeyLength(usize),
 
     /// Received KRB-ERROR response.
-    #[fail (display = "Received {}", _0)]
+    #[fail(display = "Received {}", _0)]
     KrbErrorResponse(KrbError),
 
     /// Error resolving name.
-    #[fail (display = "Error resolving name: {}", _0)]
+    #[fail(display = "Error resolving name: {}", _0)]
     NameResolutionError(String),
 
     /// Error sending/receiving data over the network.
@@ -61,11 +63,11 @@ pub enum ErrorKind {
     NetworkError,
 
     /// No key was provided in order to decrypt the KDC response.
-    #[fail (display = "No key was provided")]
+    #[fail(display = "No key was provided")]
     NoKeyProvided,
 
     /// None cipher algorithm supported was specified.
-    #[fail (display = "None cipher algorithm supported was specified")]
+    #[fail(display = "None cipher algorithm supported was specified")]
     NoProvidedSupportedCipherAlgorithm,
 
     /// Some necessary data was not available in order to build the required message.
@@ -73,7 +75,7 @@ pub enum ErrorKind {
     NotAvailableData(String),
 
     /// Error parsing KDC-REP message.
-    #[fail (display = "Error parsing KdcRep: {}", _1)]
+    #[fail(display = "Error parsing KdcRep: {}", _1)]
     ParseKdcRepError(KdcRep, Box<ErrorKind>),
 
     /// The type of the principal name was not specified.
@@ -85,20 +87,18 @@ pub enum ErrorKind {
 #[derive(Clone, PartialEq, Debug, Fail)]
 pub enum CryptographyErrorKind {
     /// Error while decrypting the data
-    #[fail (display = "Decryption error: {}", _0)]
+    #[fail(display = "Decryption error: {}", _0)]
     DecryptionError(String),
 
     /// Data is encrypted with an unsupported algorithm
-    #[fail (display = "Cipher algorithm with etype = {} is not supported", _0)]
+    #[fail(display = "Cipher algorithm with etype = {} is not supported", _0)]
     UnsupportedCipherAlgorithm(i32),
 }
 
 impl Error {
-
     pub fn kind(&self) -> &ErrorKind {
         return self.inner.get_context();
     }
-
 }
 
 impl Fail for Error {
@@ -120,7 +120,7 @@ impl fmt::Display for Error {
 impl From<ErrorKind> for Error {
     fn from(kind: ErrorKind) -> Error {
         return Error {
-            inner: Context::new(kind)
+            inner: Context::new(kind),
         };
     }
 }
@@ -134,18 +134,15 @@ impl From<Context<ErrorKind>> for Error {
 impl From<CryptographyErrorKind> for Error {
     fn from(kind: CryptographyErrorKind) -> Error {
         return Error {
-            inner: Context::new(
-                ErrorKind::CryptographyError(Box::new(kind))
-            )
+            inner: Context::new(ErrorKind::CryptographyError(Box::new(kind))),
         };
     }
 }
 
-
 impl From<FromAsciiError<&str>> for Error {
     fn from(_error: FromAsciiError<&str>) -> Self {
         return Error {
-            inner: Context::new(ErrorKind::InvalidAscii)
+            inner: Context::new(ErrorKind::InvalidAscii),
         };
     }
 }
@@ -153,28 +150,24 @@ impl From<FromAsciiError<&str>> for Error {
 impl From<red_asn1::Error> for Error {
     fn from(error: red_asn1::Error) -> Self {
         return Error {
-            inner: Context::new(ErrorKind::Asn1Error(error.kind().clone()))
+            inner: Context::new(ErrorKind::Asn1Error(error.kind().clone())),
         };
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn test_kerberos_error() {
         match produce_invalid_network_error() {
-            Err(kerberos_error) => {
-                match kerberos_error.kind() {
-                    ErrorKind::NetworkError  => {
-                        
-                    }
-                    _ => {
-                        unreachable!();
-                    }
+            Err(kerberos_error) => match kerberos_error.kind() {
+                ErrorKind::NetworkError => {}
+                _ => {
+                    unreachable!();
                 }
-            }
+            },
             _ => {
                 unreachable!();
             }
