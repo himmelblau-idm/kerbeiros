82a57780609c51c0f3c4965586fdb1c3217827a5
Eloy <zer1t0ps@protonmail.com>
2020-03-25 07:58:44 +0100

    refactor: removed credential_warehouse_ccache

--- src/credentials/mappers/mod.rs
+++ src/credentials/mappers/mod.rs
@@ -4,8 +4,5 @@ pub use credential_krb_info::*;
 mod credential_ccache;
 pub use credential_ccache::*;
 
-mod credential_warehouse_ccache;
-pub use credential_warehouse_ccache::*;
-
 mod credential_warehouse_krb_cred;
 pub use credential_warehouse_krb_cred::*;
