f2c467915a806fde659eb457d21a03a47d7ec510
Eloy <zer1t0ps@protonmail.com>
2020-05-01 13:57:26 +0200

    add kerberos_constants library

---
+++ kerberos_constants/src/lib.rs
@@ -0,0 +1,16 @@
+//! # Kerberos constants
+//! This library defines the numeric constants used across the Kerberos protocol by different objects and actors.
+
+pub mod ad_types;
+pub mod address_types;
+pub mod ap_options;
+pub mod error_codes;
+pub mod etypes;
+pub mod kdc_options;
+pub mod kerb_error_data_type;
+pub mod message_types;
+pub mod pa_data_types;
+pub mod principal_names;
+pub mod protocol_version;
+pub mod ticket_flags;
+pub mod tr_types;
---
+++ kerberos_constants/src/protocol_version.rs
@@ -0,0 +1,5 @@
+//! The protocol version of Kerberos V5
+//!
+//! Defined in RFC4120, section 7.5.6.
+
+pub const PVNO: i32 = 5;
