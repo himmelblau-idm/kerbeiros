3054a15fa3cc2b3d860b0ca90fd4cf0e432c752e
Eloy <zer1t0ps@protonmail.com>
2020-05-01 11:55:07 +0200

    add failure to kerberos_crypto error

--- kerberos_crypto/src/error.rs
+++ kerberos_crypto/src/error.rs
@@ -1,21 +1,16 @@
 use std::result;
-use std::fmt;
+use failure::Fail;
 
 /// Result that encapsulates the Error type of this library
 pub type Result<T> = result::Result<T, Error>;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Fail, Clone, Debug, PartialEq)]
 pub enum Error {
     /// Error while decrypting the data
+    #[fail(display = "DecryptionError: {}", _0)]
     DecryptionError(String),
 
     /// Data is encrypted with an unsupported crypto algorithm
+    #[fail(display = "UnsupportedAlgorithm: {}", _0)]
     UnsupportedAlgorithm(i32),
-}
-
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Display::fmt(&self, f)
-    }
 }
