6807d5d9a0ec72a869e9c7001bceb748a1f35a47
Eloy <zer1t0ps@protonmail.com>
2020-05-09 17:03:25 +0200

    change time mappers to accept and return Option<KerberosTime>

--- kerberos_ccache/src/mappers/times_mapper.rs
+++ kerberos_ccache/src/mappers/times_mapper.rs
@@ -3,27 +3,34 @@ use chrono::{TimeZone, Utc};
 use kerberos_asn1::KerberosTime;
 
 pub fn authtime_starttime_endtime_renew_till_to_times(
-    authtime: &KerberosTime,
+    authtime: Option<&KerberosTime>,
     starttime: Option<&KerberosTime>,
-    endtime: &KerberosTime,
+    endtime: Option<&KerberosTime>,
     renew_till: Option<&KerberosTime>,
 ) -> Times {
-    let authtime_timestamp = authtime.timestamp() as u32;
-    let endtime_timestamp = endtime.timestamp() as u32;
-    let starttime_timestamp;
-    let renew_till_timestamp;
-
-    if let Some(starttime) = starttime {
-        starttime_timestamp = starttime.timestamp() as u32;
+    let authtime_timestamp = if let Some(time) = authtime {
+        time.timestamp() as u32
     } else {
-        starttime_timestamp = authtime_timestamp;
-    }
+        0
+    };
 
-    if let Some(renew_till) = renew_till {
-        renew_till_timestamp = renew_till.timestamp() as u32;
+    let starttime_timestamp = if let Some(time) = starttime {
+        time.timestamp() as u32
     } else {
-        renew_till_timestamp = 0
-    }
+        0
+    };
+
+    let endtime_timestamp = if let Some(time) = endtime {
+        time.timestamp() as u32
+    } else {
+        0
+    };
+
+    let renew_till_timestamp = if let Some(time) = renew_till {
+        time.timestamp() as u32
+    } else {
+        0
+    };
 
     return Times::new(
         authtime_timestamp,
@@ -36,16 +43,33 @@ pub fn authtime_starttime_endtime_renew_till_to_times(
 pub fn times_to_authtime_starttime_endtime_renew_till(
     times: &Times,
 ) -> (
-    KerberosTime,
-    KerberosTime,
-    KerberosTime,
+    Option<KerberosTime>,
+    Option<KerberosTime>,
+    Option<KerberosTime>,
     Option<KerberosTime>,
 ) {
-    let authtime = KerberosTime::from(Utc.timestamp(times.authtime as i64, 0));
-    let starttime =
-        KerberosTime::from(Utc.timestamp(times.starttime as i64, 0));
-    let endtime = KerberosTime::from(Utc.timestamp(times.endtime as i64, 0));
+    let authtime = match times.authtime {
+        0 => None,
+        _ => Some(KerberosTime::from(
+            Utc.timestamp(times.authtime as i64, 0),
+        )),
+    };
+    
+    let starttime = match times.starttime {
+        0 => None,
+        _ => Some(KerberosTime::from(
+            Utc.timestamp(times.starttime as i64, 0),
+        )),
+    };
+    
+    let endtime = match times.endtime {
+        0 => None,
+        _ => Some(KerberosTime::from(
+            Utc.timestamp(times.endtime as i64, 0),
+        )),
+    };
 
+    
     let renew_till = match times.renew_till {
         0 => None,
         _ => Some(KerberosTime::from(
@@ -106,7 +130,10 @@ mod test {
         assert_eq!(
             time,
             authtime_starttime_endtime_renew_till_to_times(
-                Some(&authtime), None, Some(&endtime), None
+                Some(&authtime),
+                None,
+                Some(&endtime),
+                None
             )
         );
     }
@@ -130,7 +157,12 @@ mod test {
         );
 
         assert_eq!(
-            (Some(authtime), Some(starttime), Some(endtime), Some(renew_till)),
+            (
+                Some(authtime),
+                Some(starttime),
+                Some(endtime),
+                Some(renew_till)
+            ),
             times_to_authtime_starttime_endtime_renew_till(&time)
         );
     }
