eed9909ab2c2a79d05ac699c8353eb35d4e97c92
Eloy <zer1t0ps@protonmail.com>
2020-05-10 11:27:24 +0200

    move new_kerberos_cipher to its own module

--- kerberos_crypto/src/ciphers/mod.rs
+++ kerberos_crypto/src/ciphers/mod.rs
@@ -7,29 +7,7 @@ pub use aes::AESCipher;
 mod rc4;
 pub use rc4::RC4Cipher;
 
-use crate::cryptography::AesSizes;
-use kerberos_constants::etypes::{
-    AES128_CTS_HMAC_SHA1_96, AES256_CTS_HMAC_SHA1_96, RC4_HMAC,
-};
-
-use crate::{Error, Result};
-
-/// Creates the appropiate cipher based on the encryption type specified
-pub fn new_kerberos_cipher(etype: i32) -> Result<Box<dyn KerberosCipher>> {
-    match etype {
-        AES256_CTS_HMAC_SHA1_96 => {
-            return Ok(Box::new(AESCipher::new(AesSizes::Aes256)));
-        }
-        AES128_CTS_HMAC_SHA1_96 => {
-            return Ok(Box::new(AESCipher::new(AesSizes::Aes128)));
-        }
-        RC4_HMAC => {
-            return Ok(Box::new(RC4Cipher::new()));
-        }
-        _ => {
-            return Err(Error::UnsupportedAlgorithm(etype))?;
-        }
-    }
-}
+mod factory;
+pub use factory::new_kerberos_cipher;
 
 
