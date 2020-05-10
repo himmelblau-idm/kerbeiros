c8e800f1dbed77743ee7254f51549f0476e0a45e
Eloy <zer1t0ps@protonmail.com>
2020-05-10 10:57:56 +0200

    move md4 to its own file

--- kerberos_crypto/src/cryptography/mod.rs
+++ kerberos_crypto/src/cryptography/mod.rs
@@ -1,5 +1,4 @@
-use md4::{Digest, Md4};
-
+//! Module to provide low-level cryptographic routines
 
 mod aes;
 pub use aes::{decrypt_aes_ecb, encrypt_aes_cbc, pbkdf2_sha1, AesSizes};
@@ -10,7 +9,5 @@ pub use hmac::{hmac_md5, hmac_sha1};
 mod rc4;
 pub use rc4::{rc4_decrypt, rc4_encrypt};
 
-pub fn md4(bytes: &[u8]) -> Vec<u8> {
-    return Md4::digest(&bytes).to_vec();
-}
-
+mod md4lib;
+pub use md4lib::md4;
