ad3774ef021ac07e55e14c86853ed0e15942c3bb
Eloy <zer1t0ps@protonmail.com>
2020-06-13 12:26:57 +0200

    fix salt aes generation in case of hostnames clients (end with $)

--- kerberos_crypto/src/algorithms/aes_hmac_sha1/salt.rs
+++ kerberos_crypto/src/algorithms/aes_hmac_sha1/salt.rs
@@ -4,10 +4,15 @@ pub fn generate_salt(realm: &str, client_name: &str) -> Vec<u8> {
     let mut lowercase_username = client_name.to_lowercase();
 
     if lowercase_username.ends_with("$") {
+        // client name = "host<client_name>.lower.domain.com"
         salt.push_str("host");
         lowercase_username.pop();
+        salt.push_str(&lowercase_username);
+        salt.push('.');
+        salt.push_str(&realm.to_lowercase());
+    } else {
+        salt.push_str(&lowercase_username);
     }
-    salt.push_str(&lowercase_username);
 
     return salt.as_bytes().to_vec();
 }
@@ -27,7 +32,7 @@ mod test {
     #[test]
     fn test_generate_aes_host_salt() {
         assert_eq!(
-            "KINGDOM.HEARTShostpc".as_bytes().to_vec(),
+            "KINGDOM.HEARTShostpc.kingdom.hearts".as_bytes().to_vec(),
             generate_salt("kingdom.Hearts", "PC$")
         );
     }
