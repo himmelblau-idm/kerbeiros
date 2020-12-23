d0715ecf115d39abeae1018c93682a38bd7d8c44
Eloy Pérez <zer1t0ps@protonmail.com>
2020-12-23 09:39:17 +0100

    add context tag to PaPacOptions

--- kerberos_asn1/src/pa_data/pa_pac_options.rs
+++ kerberos_asn1/src/pa_data/pa_pac_options.rs
@@ -15,5 +15,41 @@ use red_asn1_derive::Sequence;
 /// ```
 #[derive(Sequence, Default, Debug, Clone, PartialEq)]
 pub struct PaPacOptions {
+    #[seq_field(context_tag = 0)]
     pub kerberos_flags: KerberosFlags,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_pa_pac_options() {
+        let options = PaPacOptions {
+            kerberos_flags: 0x10000000.into(),
+        };
+        assert_eq!(
+            vec![
+                0x30, 0x09, 0xa0, 0x07, 0x03, 0x05, 0x00, 0x10, 0x00, 0x00,
+                0x00
+            ],
+            options.build()
+        )
+    }
+
+    #[test]
+    fn test_parse_pa_pac_options() {
+        let options = PaPacOptions {
+            kerberos_flags: 0x10000000.into(),
+        };
+        assert_eq!(
+            options,
+            PaPacOptions::parse(&[
+                0x30, 0x09, 0xa0, 0x07, 0x03, 0x05, 0x00, 0x10, 0x00, 0x00,
+                0x00
+            ])
+            .unwrap()
+            .1
+        )
+    }
 }
