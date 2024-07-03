4f3537e37f89d21eb1533aef11e8e5b78304c248
David Mulder <dmulder@samba.org>
2024-07-03 11:23:40 -0600

    Stop using the failure crate
    This crate has known vulnerabilities and should
    not be used.
    
    Signed-off-by: David Mulder <dmulder@samba.org>

--- kerbeiros/src/error.rs
+++ kerbeiros/src/error.rs
@@ -2,92 +2,92 @@
 
 use crate::messages::{AsRep, KrbError};
 use ascii::FromAsciiError;
-use failure::Fail;
 use std::result;
 use std::string::FromUtf8Error;
+use thiserror::Error;
 
 /// Result to wrap kerbeiros error.
 pub type Result<T> = result::Result<T, Error>;
 
 /// Type of error in kerbeiros library.
-#[derive(Clone, PartialEq, Debug, Fail)]
+#[derive(Clone, PartialEq, Debug, Error)]
 pub enum Error {
     /// Error handlening asn1 entities.
-    #[fail(display = "Asn1 error: {}", _0)]
+    #[error("Asn1 error: {}", _0)]
     Asn1Error(himmelblau_kerberos_asn1::Error),
 
     /// Error produced in the application of cryptographic algorithms.
-    #[fail(display = "Cryptography error: {}", _0)]
+    #[error("Cryptography error: {}", _0)]
     CryptographyError(himmelblau_kerberos_crypto::Error),
 
     /// Invalid ascii string.
-    #[fail(display = "Invalid ascii string")]
+    #[error("Invalid ascii string")]
     InvalidAscii,
 
     /// Invalid utf8 string.
-    #[fail(display = "Invalid utf-8 string")]
+    #[error("Invalid utf-8 string")]
     InvalidUtf8,
 
     /// Invalid microseconds value. Minimum = 0, Maximum = 999999.
-    #[fail(display = "Invalid microseconds value {}. Max is 999999", _0)]
+    #[error("Invalid microseconds value {}. Max is 999999", _0)]
     InvalidMicroseconds(u32),
 
     /// Error in i/o operation.
-    #[fail(display = "Error in i/o operation")]
+    #[error("Error in i/o operation")]
     IOError,
 
     /// Invalid key
-    #[fail(
-        display = "Invalid key: Only hexadecimal characters are allowed [1234567890abcdefABCDEF]"
+    #[error(
+        "Invalid key: Only hexadecimal characters are allowed [1234567890abcdefABCDEF]"
     )]
     InvalidKeyCharset,
 
     /// Invalid key
-    #[fail(display = "Invalid key: Length should be {}", _0)]
+    #[error("Invalid key: Length should be {}", _0)]
     InvalidKeyLength(usize),
 
     /// Received KRB-ERROR response.
-    #[fail(display = "Received {:?}", _0)]
+    #[error("Received {:?}", _0)]
     KrbErrorResponse(KrbError),
 
     /// Error resolving name.
-    #[fail(display = "Error resolving name: {}", _0)]
+    #[error("Error resolving name: {}", _0)]
     NameResolutionError(String),
 
     /// Error sending/receiving data over the network.
-    #[fail(display = "Network error")]
+    #[error("Network error")]
     NetworkError,
 
     /// No key was provided in order to decrypt the KDC response.
-    #[fail(display = "No key was provided")]
+    #[error("No key was provided")]
     NoKeyProvided,
 
     /// None cipher algorithm supported was specified.
-    #[fail(display = "None cipher algorithm supported was specified")]
+    #[error("None cipher algorithm supported was specified")]
     NoProvidedSupportedCipherAlgorithm,
 
     /// Some necessary data was not available in order to build the required message.
-    #[fail(display = "Not available data {}", _0)]
+    #[error("Not available data {}", _0)]
     NotAvailableData(String),
 
     /// Error parsing AS-REP message.
-    #[fail(display = "Error parsing AsRep: {}", _1)]
+    #[error("Error parsing AsRep: {}", _1)]
     ParseAsRepError(AsRep, Box<Error>),
 
     /// The type of the principal name was not specified.
-    #[fail(display = "Undefined type of principal name: {}", _0)]
+    #[error("Undefined type of principal name: {}", _0)]
     PrincipalNameTypeUndefined(String),
 
     /// No principal name
-    #[fail(display = "No principal name found")]
+    #[error("No principal name found")]
     NoPrincipalName,
 
     /// No address found
-    #[fail(display = "No address found")]
+    #[error("No address found")]
     NoAddress,
 
     /// Error parsing binary data
-    #[fail(display = "Error parsing binary data")]
+    #[error("Error parsing binary data")]
     BinaryParseError,
 }
 
